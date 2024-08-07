656ac07e4707d807fa2f82316d0406f2ecd2930e
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-08-07 13:13:13 +0900

    feat: 데이터 바인딩 에러 처리 추가

--- src/main/java/com/wanted/pre_onboarding/advice/ValidationExceptionHandler.java
+++ src/main/java/com/wanted/pre_onboarding/advice/ValidationExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.wanted.pre_onboarding.advice;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.wanted.pre_onboarding.dto.response.ValidationErrorResponse;
 import com.wanted.pre_onboarding.dto.response.ValidationResult;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,12 +21,35 @@ public class ValidationExceptionHandler {
         BindingResult bindingResult = ex.getBindingResult();
         List<ValidationResult> errors = bindingResult.getFieldErrors()
                 .stream()
-                .map(fieldError -> new ValidationResult(
-                        fieldError.getField(),
-                        String.valueOf(fieldError.getRejectedValue()),
-                        fieldError.getDefaultMessage())
-                )
+                .map(fieldError -> ValidationResult.builder()
+                        .field(fieldError.getField())
+                        .rejectedValue(String.valueOf(fieldError.getRejectedValue()))
+                        .message(fieldError.getDefaultMessage())
+                        .build())
                 .collect(Collectors.toList());
         return ResponseEntity.badRequest().body(new ValidationErrorResponse(errors));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handle(HttpMessageNotReadableException e) {
+        if (e.getCause() instanceof InvalidFormatException ex) {
+
+            String fieldName = ex.getPath().get(0).getFieldName();
+            String rejectedValue = String.valueOf(ex.getValue());
+            ValidationResult errors = ValidationResult.builder()
+                    .field(fieldName)
+                    .rejectedValue(rejectedValue)
+                    .message("Invalid Format")
+                    .build();
+            return ResponseEntity.badRequest().body(new ValidationErrorResponse(List.of(errors)));
+        }
+
+        ValidationResult defaultError = ValidationResult.builder()
+                .field("unknown")
+                .rejectedValue("unknown")
+                .message("Malformed JSON request")
+                .build();
+
+        return ResponseEntity.badRequest().body(new ValidationErrorResponse(Collections.singletonList(defaultError)));
     }
 }
--- src/main/java/com/wanted/pre_onboarding/dto/response/ValidationResult.java
+++ src/main/java/com/wanted/pre_onboarding/dto/response/ValidationResult.java
@@ -1,14 +1,19 @@
 package com.wanted.pre_onboarding.dto.response;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class ValidationResult {
     private String field;
     private String rejectedValue;
     private String message;
+    @Builder
+    public ValidationResult(String field, String rejectedValue, String message) {
+        this.field = field;
+        this.rejectedValue = rejectedValue;
+        this.message = message;
+    }
 }
