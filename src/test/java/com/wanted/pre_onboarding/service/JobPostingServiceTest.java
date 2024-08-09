734775fe6b03ed705bbbd9f8975a67f2e8c4489a
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-08-09 18:18:29 +0900

    test: 채용공고 목록 조회 테스트 코드 추가

--- src/test/java/com/wanted/pre_onboarding/service/JobPostingServiceTest.java
+++ src/test/java/com/wanted/pre_onboarding/service/JobPostingServiceTest.java
@@ -5,6 +5,7 @@ import com.wanted.pre_onboarding.domain.Company;
 import com.wanted.pre_onboarding.domain.JobPosting;
 import com.wanted.pre_onboarding.dto.request.JobPostingRequest;
 import com.wanted.pre_onboarding.dto.response.JobPostingResponse;
+import com.wanted.pre_onboarding.dto.response.JobPostingSummary;
 import com.wanted.pre_onboarding.exception.EntityNotFoundException;
 import com.wanted.pre_onboarding.repository.CompanyRepository;
 import com.wanted.pre_onboarding.repository.JobPostingRepository;
@@ -18,6 +19,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -132,5 +134,50 @@ class JobPostingServiceTest {
         assertThatThrownBy(() -> jobPostingService.delete(1L))
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessageContaining("요청한 채용공고를 찾지 못했습니다.");
+    }
+
+    @Test
+    @DisplayName("공개된 채용 공고만 목록에 포함되는지 확인")
+    void shouldReturnOnlyOpenJobPostings_WhenListIsCalled() {
+        // given
+        Company wantedLab = companyRepository.findById(1L)
+                .orElseThrow(EntityNotFoundException::new);
+        Company naver = companyRepository.findById(2L)
+                .orElseThrow(EntityNotFoundException::new);
+
+        JobPosting jobPosting2 = JobPosting.builder()
+                .position("Java 개발자")
+                .description("원티드랩에서 자바 개발자를 채용합니다.")
+                .reward(1000000)
+                .usedSkills(Set.of("Python", "Django"))
+                .company(wantedLab)
+                .build();
+        jobPostingRepository.save(jobPosting2);
+
+        JobPosting jobPosting3 = JobPosting.builder()
+                .position("백엔드 개발자")
+                .description("네이버에서 백엔드 개발자를 채용합니다.")
+                .reward(1000000)
+                .usedSkills(Set.of("Python", "Go"))
+                .company(naver)
+                .build();
+        jobPosting3.updateOpenStatus(false);
+        jobPostingRepository.save(jobPosting3);
+
+        // when
+        List<JobPostingSummary> summaries = jobPostingService.list();
+        // Then
+        assertThat(summaries).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("채용공고 없는 경우 빈 리스트 반환")
+    void shouldReturnEmptyList_WhenListIsCalled() {
+        // given
+        jobPostingRepository.deleteAll();
+        // when
+        List<JobPostingSummary> summaries = jobPostingService.list();
+        // Then
+        assertThat(summaries).isEmpty();
     }
 }
